4e123938f9c61ae67a99f31c6d1c4f786fe4d7db
SixtoCalvario <131615750+SixtoCalvario@users.noreply.github.com>
2025-02-01 00:41:57 +0000

    these

---
+++ Driver.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Driver{
+    public static void main(String[] args) {
+    
+        JFrame frame = new JFrame();
+        frame.setSize(1600, 900);
+        frame.setVisible(true);
+        
+    }
+
+}
